8fb5fd76401d4db016f5cb02d7948121bf2973f4
Mauricio Poppe <mauricio.poppe@gmail.com>
2020-09-13 23:05:01 -0700

    use NODE_ENV production

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
-  mode: process.env.NODE_ENV === 'PROD' ? 'production' : 'development',
-  devtool: process.env.NODE_ENV === 'PROD' ? 'nosources-source-map' : 'inline-source-map',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: process.env.NODE_ENV === 'production' ? 'nosources-source-map' : 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'function-plot.js',
