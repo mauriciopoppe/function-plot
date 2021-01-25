b31228689fe821095dfdfde4a71d7075282705ec
Dan Jutan <danjutan@gmail.com>
2021-01-24 20:14:31 -0500

    fix external

--- src/external.d.ts
+++ src/external.d.ts
@@ -1,15 +1,6 @@
 declare module 'clamp' {
   export default function clamp(lo: number, hi: number, n: number): number
 }
-declare module 'linspace' {
-  export default function linspace(lo: number, hi: number, n: number): number[]
-}
-declare module 'logspace' {
-  export default function logspace(lo: number, hi: number, n: number): number[]
-}
-declare module 'log10' {
-  export default function log10(n: number): number
-}
 
 declare module 'built-in-math-eval' {
   export default function eval(expr: string): any
