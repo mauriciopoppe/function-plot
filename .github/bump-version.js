bc71cfcfbdd0e4a686c97bd9d6329c03a93a5440
leonhma <leonhardmasche@gmail.com>
2022-08-22 18:27:51 +0200

    improvement(automation): release workflow and improved ci

---
+++ .github/bump-version.js
@@ -0,0 +1,11 @@
+import { readFileSync, writeFileSync } from "fs";
+
+const version = process.argv[2];
+
+// read files
+let package_ = JSON.parse(readFileSync("package.json", "utf8"));
+
+// update versions
+package_.version = version;
+
+writeFileSync("package.json", JSON.stringify(package_, null, "\t"));
