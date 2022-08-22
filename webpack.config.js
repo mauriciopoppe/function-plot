3af1f1846b892234b444f4f4437bc4ae1d3795ba
leonhma <leonhardmasche@gmail.com>
2022-08-22 20:46:27 +0200

    feat(automation): deploy to gh-pages on release

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const path = require('path')
 module.exports = {
   entry: './src/index.ts',
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  devtool: process.env.NODE_ENV === 'production' ? 'nosources-source-map' : 'inline-source-map',
+  devtool: process.env.NODE_ENV === 'production' ? false : 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
     filename: 'function-plot.js',
     library: 'functionPlot',
     libraryExport: 'default',
