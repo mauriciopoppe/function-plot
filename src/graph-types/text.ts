f025af7f376cf403677620521175189b0e7b7143
Mauricio Poppe <mauricio.poppe@gmail.com>
2023-12-15 23:07:45 -0500

    feat: new graph type text

---
+++ src/graph-types/text.ts
@@ -0,0 +1,44 @@
+import { select as d3Select, Selection } from 'd3-selection'
+import { hsl as d3Hsl } from 'd3-color'
+
+import utils from '../utils'
+
+import { Chart } from '../index'
+import { FunctionPlotDatum } from '../types'
+
+export default function Text(chart: Chart) {
+  const xScale = chart.meta.xScale
+  const yScale = chart.meta.yScale
+
+  function text(selection: Selection<any, FunctionPlotDatum, any, any>) {
+    selection.each(function (d) {
+      // Force some parameters to make it look like a vector.
+      d.sampler = 'builtIn'
+      d.fnType = 'vector'
+
+      const innerSelection = d3Select(this).selectAll(':scope > text.fn-text').data([d.location])
+      const innerSelectionEnter = innerSelection.enter().append('text').attr('class', `fn-text fn-text-${d.index}`)
+
+      const color = utils.color(d, d.index)
+
+      // enter + update
+      const selection = innerSelection
+        .merge(innerSelectionEnter)
+        .attr('fill', d3Hsl(color.toString()).brighter(1.5).formatHex())
+        .attr('x', (d) => xScale(d[0]))
+        .attr('y', (d) => yScale(d[1]))
+        .text(() => d.text)
+
+      if (d.attr) {
+        for (const k in d.attr) {
+          selection.attr(k, d.attr[k])
+        }
+      }
+
+      // exit
+      innerSelection.exit().remove()
+    })
+  }
+
+  return text
+}
