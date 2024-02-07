ca6634deae63b279a1c1b23aa4393817cde0e599
Mauricio Poppe <mauricio.poppe@gmail.com>
2024-02-07 18:47:18 -0500

    fix: bump package.json version to v1.24.1 to prepare it for 1.24.2
    1.24.1 is a bad release, fixing package.json in preparation for 1.24.2

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,6 @@
 const config = {
   preset: 'jest-puppeteer',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '<rootDir>/{src,test}/**/?(*.)+(spec|test).[jt]s?(x)'],
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!d3-\\.*|internmap)']
 }
 
