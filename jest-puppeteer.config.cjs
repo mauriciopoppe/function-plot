5239495da39184b7fe3738122358debc9b0c8c43
Mauricio Poppe <mauricio.poppe@gmail.com>
2026-05-02 11:33:54 -0400

    chore: bump dependencies

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   server: {
-    command: 'webpack-dev-server --port 4444',
-    port: 4444
+    command: 'webpack-dev-server --port 4444 --host 127.0.0.1',
+    port: 4444,
+    launchTimeout: 60000
   }
 }
